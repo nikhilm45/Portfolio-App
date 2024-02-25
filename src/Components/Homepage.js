97b1f6f323bd7ebc0569ef185fec66b160eec2c6
nikhilm45 <marathen37@gmail.com>
2024-02-25 20:48:16 +0530

    Dev: Code Skeleton

---
+++ src/Components/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <div className="homepage"></div>
+        <p>Homepage</p>
+      </div>
+    );
+  }
+}
